695387dacc58e41b1228a15371cb91b275438809
shashank-kenpath <104070563+shashank-kenpath@users.noreply.github.com>
2025-05-30 12:21:03 +0530

    keycloak mapping

--- src/lib/keycloak.ts
+++ src/lib/keycloak.ts
@@ -2,10 +2,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  // url: 'http://localhost:8888/',
-  // url:  'http://192.168.100.195:8888/',
-  url:  'https://accounts.vistaar.kenpath.ai/auth',
-//   url:  'https://accounts.vistaar.kenpath.ai/auth/',
+  url:  'https://auth-oan.kenpath.ai/auth',
   realm: 'Vistaar',
   clientId: 'vistaar-ui',
 });
